f6281f78cdb048740b872dc7dbe0ce7f0cc99612
Abuaisha-7 <heyrubedru8086@gmail.com>
2026-01-04 17:27:20 +0300

    adding Routes handling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,36 @@
+import {Routes, Route} from 'react-router-dom'
+import {SignedIn, SignedOut, RedirectToSignIn} from "@clerk/clerk-react"
+import HomePage from './pages/HomePage'
+import SignInPage from './pages/SignInPage'
+import SignUpPage from './pages/SignUp'
+import DashboardPage from './pages/DashboardPage'
+import PricingPage from './pages/PricingPage'
 
+function ProtectedRoute ({children}) {
+    return <>
+        <SignedIn>{children}</SignedIn>
+        <SignedOut>
+            <RedirectToSignIn />
+        </SignedOut>
+</>
+}
 
 function App() {
+    return <Routes>
+        <Route path='/'>
+            <Route index element= {<HomePage />} />
+            <Route path={"sign-in/*"} element={<SignInPage />} />
+            <Route path={"sign-up/*"} element={<SignUpPage />} />
+            <Route path={"pricing"} element={<PricingPage />} />
+            <Route 
+              path={"dashboard"} 
+              element={
+                <ProtectedRoute>
+                    <DashboardPage/>
+                </ProtectedRoute>
+              } />
+        </Route>
+    </Routes>
  
 }
 
